ea194172f2eacdce05116f612aae02f21273f660
VitoColet <vitohugocoleta2005@gmail.com>
2025-10-20 20:05:08 -0300

    criado LivroController

--- src/main/java/controller/LivroController.java
+++ src/main/java/controller/LivroController.java
@@ -1,4 +1,35 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import resources.entitys.LivroEntity;
+import service.LivroService;
+
+@RestController
+@RequestMapping("/livro")
 public class LivroController {
+
+    private final LivroService service;
+
+    public LivroController(LivroService service) {
+        this.service = service;
+    }
+
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<LivroEntity> getRequest(@PathVariable Integer id){
+        return ResponseEntity.ok(service.Find(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postRequest(@RequestBody LivroEntity livro){
+        service.Create(livro);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping (value = "/id")
+    public ResponseEntity<Void> putRequest(@PathVariable Integer id, @RequestBody LivroEntity livro){
+        service.Update(id, livro);
+        return ResponseEntity.ok().build();
+    }
 }
