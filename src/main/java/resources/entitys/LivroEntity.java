258ccf43187eccf2fcf94e124cd868ac334765e0
VitoColet <vitohugocoleta2005@gmail.com>
2025-10-20 19:32:51 -0300

    criado livroEntity

---
+++ src/main/java/resources/entitys/LivroEntity.java
@@ -0,0 +1,28 @@
+package resources.entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LivroEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int anoLancamento;
+
+}
